915af34fd786ef584c0a3f16e58f8a57cd2a725c
Richard Ouyang <richardlouyang@gmail.com>
2017-01-19 22:35:37 -0600

    removed appearing on desktop thing

--- hypbioform.py
+++ hypbioform.py
@@ -7,8 +7,6 @@ import re
 import time
 import wget
 
-home = os.path.expanduser("~")
-
 #### SENIORS ####
 def remove_brackets(string):
     return re.sub('[ ][\[].*?[\]]', '', string)
@@ -127,11 +125,11 @@ def get_groups_info(row):
 #### END PROFS ####
 
 def download_seniors():
-    if os.path.exists(home + '/Desktop/Senior-Bioform.csv'):
-        os.remove(home + '/Desktop/Senior-Bioform.csv')
+    if os.path.exists('Senior-Bioform.csv'):
+        os.remove('Senior-Bioform.csv')
 
-    wget.download('https://www.jotform.com/csv/70045832837054', home + '/Desktop/Senior-Bioform.csv')
-    seniors = pd.read_csv(home + '/Desktop/Senior-Bioform.csv', dtype=str)
+    wget.download('https://www.jotform.com/csv/70045832837054', 'Senior-Bioform.csv')
+    seniors = pd.read_csv('Senior-Bioform.csv', dtype=str)
 
     # replace all NaNs
     seniors = seniors.fillna('')
@@ -148,16 +146,16 @@ def get_seniors():
     seniors = seniors.apply(lambda row: get_senior_info(row), axis=1)
     seniors.columns = ['fullname', 'bio', 'house', 'first_name', 'last_name', 'email', 'time_submitted']
     
-    os.remove(home + '/Desktop/Senior-Bioform.csv')
+    os.remove('Senior-Bioform.csv')
 
     return seniors.to_csv(index_label='id')
 
 def get_groups():
-    if os.path.exists(home + '/Desktop/Group-Info.csv'):
-        os.remove(home + '/Desktop/Group-Info.csv')
-    wget.download('https://www.jotform.com/csv/70047800855051', home + '/Desktop/Group-Info.csv')
+    if os.path.exists('Group-Info.csv'):
+        os.remove('Group-Info.csv')
+    wget.download('https://www.jotform.com/csv/70047800855051', 'Group-Info.csv')
 
-    groups = pd.read_csv(home + '/Desktop/Group-Info.csv', dtype=str)
+    groups = pd.read_csv('Group-Info.csv', dtype=str)
 
     # replace all NaNs
     groups = groups.fillna('')
@@ -169,7 +167,7 @@ def get_groups():
     groups = groups.apply(lambda row: get_groups_info(row), axis=1)
     groups.columns = ['name', 'blurb', 'officers', 'time_submit']
     
-    os.remove(home + '/Desktop/Group-Info.csv')
+    os.remove('Group-Info.csv')
 
     return groups.to_csv(index_label='id')
 
@@ -195,7 +193,7 @@ def get_profs():
     profs = process_profs()
     profs.columns = ['first_name', 'last_name', 'email', 'dept']
     
-    os.remove(home + '/Desktop/Senior-Bioform.csv')
+    os.remove('Senior-Bioform.csv')
 
     return profs.to_csv()   
 
@@ -204,13 +202,6 @@ def get_prof_counts():
     profs = process_profs()
     prof_counts = profs['last_name'].value_counts()
     
-    os.remove(home + '/Desktop/Senior-Bioform.csv')
+    os.remove('Senior-Bioform.csv')
 
     return prof_counts.to_csv()
-
-# print '\nFormatted CSVs of the following are now in your Desktop folder:'
-# print 'senior bioforms (bioforms.csv)'
-# print 'group bioforms (groups.csv)'
-# print 'professors (profs.csv)'
-# print 'professors\' last names ordered by popularity (prof_ranks.csv)'
-# print 'Close this window to continue.'
